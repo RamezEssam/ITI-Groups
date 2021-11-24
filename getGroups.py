e3fa8b90176140c64c2d114681bfea94f7a58c11
RamezEssam <56796944+RamezEssam@users.noreply.github.com>
2021-11-24 13:48:14 +0200

    Add files via upload

--- getGroups.py
+++ getGroups.py
@@ -14,16 +14,22 @@ for element in namesList:
 
 # splitting groups
 membersPerGroup = 4
-allGroups = []
-groupNum = 0
+allGroups = {}
+groupNum = 1
 while len(allNames) > 0:
-    group = [f'Group {groupNum}:']
+    group = []
     for i in range(min(membersPerGroup, len(allNames))):
         group.append(allNames.pop(random.randrange(0, len(allNames))))
-    allGroups.append(group)
+    allGroups[f'Group {groupNum}'] = group
     groupNum += 1
+
+# assigning remaining group members
+allGroups[f'Group {random.randint(1, 8)}'].append(allGroups['Group 10'].pop())
+allGroups[f'Group {random.randint(1, 8)}'].append(allGroups['Group 10'].pop())
+allGroups.pop('Group 10')
 
 # writing results in a csv file
 with open('./groups.csv', 'w') as f:
     writer = csv.writer(f)
-    writer.writerows(allGroups)
+    for key, value in allGroups.items():
+        writer.writerow([key, value])
